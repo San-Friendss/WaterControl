050c4b10013f3b3f137ff357c631b1c945baa128
FernPK <pornnapat5563@gmail.com>
2024-02-23 13:09:54 +0700

    WaterControl completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,55 @@
+// Blynk App
+#define BLYNK_AUTH_TOKEN_WATER_PUMP "0BVxluy8unor3WZHVLYNys90EzRM5IZi"
+#define BLYNK_PRINT Serial
+
+// include the libraries
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PUMP 18
+#define LED 2
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Your WiFi credentials.
+char ssid[] = "pornnapat";
+char pass[] = "0961514599";
+
+int pinValue;
+
+BLYNK_WRITE(V0)
+{
+    pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+    if (pinValue == 1)
+    {
+        digitalWrite(PUMP, HIGH);
+    }
+    else
+    {
+        digitalWrite(PUMP, LOW);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+    // Change Web Link Button message to "Congratulations!"
+    Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+    Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+    Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+    digitalWrite(LED, HIGH);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void setup()
+{
+    Serial.begin(9600);
+    Blynk.begin(BLYNK_AUTH_TOKEN_WATER_PUMP, ssid, pass, "blynk.iot-cm.com", 8080); //Blynk
+    pinMode(LED, OUTPUT);
+    pinMode(PUMP, OUTPUT);
+}
+
+void loop()
+{
+    Blynk.run(); // Blynk
+    Serial.println(pinValue);
 }
